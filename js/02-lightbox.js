ebff5f61c9826dc64e0cab3ceb1a3a17c1740313
Rublevska <73138546+Rublevska@users.noreply.github.com>
2023-08-04 23:46:21 +0300

    02 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const ulEl = document.querySelector(".gallery");
+ulEl.insertAdjacentHTML("beforeend", generateGallery(galleryItems));
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+function generateGallery(galleryItems) {
+  return galleryItems
+    .map(
+      ({ description, original, preview }) =>
+        `<li class="gallery__item">
+        <a class="gallery__link" href=${original}>
+            <img
+            class="gallery__image"
+            src=${preview}
+            alt=${description}
+            />
+        </a>
+    </li>`
+    )
+    .join("");
+}
